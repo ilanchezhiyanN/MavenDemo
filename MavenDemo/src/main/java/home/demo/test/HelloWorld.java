5c418832028e6a103e277a877ed331d4479d2505
ilanchezhiyann <chezhiyan.cse@gmail.com>
2017-11-22 03:17:51 +0530

    First Commit

---
+++ MavenDemo/src/main/java/home/demo/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package home.demo.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Maven..Hellow World");
+
+	}
+
+}
